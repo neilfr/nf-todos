192a3c37d890bc033cf0de87b7679675607b1fd2
Neil Froggatt <neilfr@live.com>
2022-03-10 22:29:36 -0500

    create base layout with initial page

---
+++ src/layouts/Base.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageOne from "../pages/PageOne";
+
+const Base: React.FC = () => {
+    return (
+        <div className="base-layout">
+            <h1>Base Layout Header</h1>
+            <PageOne/>
+        </div>
+    )
+}
+export default Base
---
+++ src/pages/PageOne.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageOne: React.FC = () => {
+    return (
+        <>
+            <h1>page one header</h1>
+            <p>this is my page one text</p>
+        </>
+    )
+}
+
+export default PageOne
