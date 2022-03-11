7d7615b0a4d0da9528fb819e356426dce54e361f
Neil Froggatt <neilfr@live.com>
2022-03-10 22:38:21 -0500

    my first react component

---
+++ src/components/MyFirstComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const MyFirstComponent:React.FC = () => {
+    return (
+        <button>my first component button</button>
+    )
+}
+
+export default MyFirstComponent
--- src/pages/PageOne.tsx
+++ src/pages/PageOne.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import MyFirstComponent from "../components/MyFirstComponent";
 
 const PageOne: React.FC = () => {
     return (
         <>
             <h1>page one header</h1>
             <p>this is my page one text</p>
+            <MyFirstComponent/>
         </>
     )
 }
