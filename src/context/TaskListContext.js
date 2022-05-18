6b85d00e6f28be458fa730ff0abd8cbf29b45b8a
Neil Froggatt <neilfr@live.com>
2022-05-18 18:43:36 -0400

    implement sorting in reducer and remove unused methods

--- src/context/TaskListContext.js
+++ src/context/TaskListContext.js
@@ -1,4 +1,4 @@
-import React, {useState, createContext, useEffect, useReducer} from "react";
+import React, {useState, createContext, useReducer} from "react";
 
 export const TaskListContext = createContext([])
 
@@ -15,17 +15,34 @@ export const defaultTask = {
     complete: false
 }
 
+const descriptionSort = (a,b) => {
+    if(a.description>b.description) return 1
+    if(a.description<b.description) return -1
+    return 0
+}
+
+const completeSort = (a,b) => {
+    if(a.complete>b.complete) return 1
+    if(a.complete<b.complete) return -1
+    return 0
+}
+
+const prioritySort = (a, b) => {
+    if(parseInt(a.priority)>parseInt(b.priority)) return 1
+    if(parseInt(a.priority)<parseInt(b.priority)) return -1
+    return 0
+}
+
 const reducer = (state,action) => {
     switch (action.type) {
         case actions.UPDATE:
             return state.map((task)=>{
                 return task.id === action.data.id ? action.data : task
-            })
+            }).sort(descriptionSort).sort(prioritySort).sort(completeSort)
         case actions.CREATE:
-            const newTask = {...action.data, id:state.length}
             return [
-                ...state, newTask
-            ]
+                ...state, {...action.data, id:state.length}
+            ].sort(descriptionSort).sort(prioritySort).sort(completeSort)
         case actions.DELETE:
             return state.filter((task)=>{
                 return task.id !== action.data.id
@@ -40,28 +57,7 @@ export const TaskListProvider = ({
 }) => {
     const [getTaskList, setTaskList] = useState([])
 
-    const updateOrCreateTask = (taskToCreateOrUpdate) => {
-        taskToCreateOrUpdate.id === null ? createNewTask(taskToCreateOrUpdate) : updateTaskInList(taskToCreateOrUpdate)
-    }
-
-    const removeTask = (taskToRemove) => {
-        setTaskList(getTaskList.filter( (task) => {
-            return task.id !== taskToRemove.id
-        }))
-    }
-
-    const createNewTask = (taskToCreateOrUpdate) => {
-        const newTask = {...taskToCreateOrUpdate, id:getTaskList.length}
-        setTaskList([...getTaskList, newTask])
-    }
-
     const [state, dispatch] = useReducer(reducer, '')
-
-    const [getSortedTaskList, setSortedTaskList] = useState(getTaskList)
-
-    useEffect(() => {
-        setSortedTaskList(sortTaskList)
-    }, [getTaskList])
 
     const getDefaultTask = () => {
         return {
@@ -72,26 +68,6 @@ export const TaskListProvider = ({
         }
     }
 
-    const sortTaskList = () => {
-        const descriptionSortedTaskList = () => {
-            return getTaskList.sort(descriptionSort)
-        }
-        const prioritySortedTaskList = () => {
-            return descriptionSortedTaskList().sort(prioritySort)
-        }
-        const completeSortedTaskList = () => {
-            return prioritySortedTaskList().sort(completeSort)
-        }
-
-        return completeSortedTaskList()
-    }
-
-    const updateTaskInList = (taskToCreateOrUpdate) => {
-        setTaskList(getTaskList.map((task) => {
-            return task.id === taskToCreateOrUpdate.id ? taskToCreateOrUpdate : task
-        }))
-    }
-
     const updateTaskCompleteStatus = (taskId) => {
         const updatedTaskList = getTaskList.map((task)=>{
             return taskId === task.id ? {...task, complete:!task.complete} : task
@@ -99,26 +75,8 @@ export const TaskListProvider = ({
         setTaskList(updatedTaskList)
     }
 
-    const completeSort = (a,b) => {
-        if(a.complete>b.complete) return 1
-        if(a.complete<b.complete) return -1
-        return 0
-    }
-
-    const prioritySort = (a, b) => {
-        if(parseInt(a.priority)>parseInt(b.priority)) return 1
-        if(parseInt(a.priority)<parseInt(b.priority)) return -1
-        return 0
-    }
-
-    const descriptionSort = (a,b) => {
-        if(a.description>b.description) return 1
-        if(a.description<b.description) return -1
-        return 0
-    }
-
     return (
-        <TaskListContext.Provider value={{getTaskList, removeTask, getSortedTaskList, updateTaskCompleteStatus, updateOrCreateTask, getDefaultTask, state, dispatch, actions}}>
+        <TaskListContext.Provider value={{getTaskList, updateTaskCompleteStatus, getDefaultTask, state, dispatch, actions}}>
             {children}
         </TaskListContext.Provider>
     )
