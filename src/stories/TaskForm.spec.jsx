4626eb1b8323e4dbca052bedc714e4a7711e7147
Neil Froggatt <neilfr@live.com>
2022-05-24 22:03:59 -0400

    refactor tests with beforeEach

--- src/stories/TaskForm.spec.jsx
+++ src/stories/TaskForm.spec.jsx
@@ -1,43 +1,67 @@
 import {TaskForm} from "./TaskForm"
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import {MemoryRouter} from "react-router-dom"
 import React from 'react'
 import {fireEvent} from "@testing-library/dom";
 
-describe('TaskForm tests', () => {
-    it('disables the save button if the description is empty', () => {
-        const { getByText, getByLabelText } = render(
-            <MemoryRouter initialEntries={
-                [
-                    {
-                        pathname:'/',
-                        state:
-                            {
-                                task:
-                                    {
-                                        id:null,
-                                        priority:1,
-                                        description:'',
-                                        complete: false
-                                    }
-                            }
-                    }
-                ]
-            }>
-                <TaskForm/>
-            </MemoryRouter>
-        )
+const renderTaskForm = () => {
+    return render(
+        <MemoryRouter initialEntries={
+            [
+                {
+                    pathname:'/',
+                    state:
+                        {
+                            task:
+                                {
+                                    id:null,
+                                    priority:1,
+                                    description:'',
+                                    complete: false
+                                }
+                        }
+                }
+            ]
+        }>
+            <TaskForm/>
+        </MemoryRouter>
+    );
+};
 
+describe('TaskForm tests', () => {
+    let getByText, getByLabelText
+
+    beforeEach(() => {
+        ({getByText, getByLabelText} = renderTaskForm());
+    });
+
+    it('disables the save button if the description is empty', () => {
         const saveButton = getByText('Save')
 
         expect(saveButton).toBeVisible();
         expect(saveButton).toHaveAttribute('disabled')
+    })
 
+    it('enables the save button if the user enters a description', () => {
+        const saveButton = getByText('Save')
         const description = getByLabelText('Description:')
-        expect(description).toHaveValue('')
 
         fireEvent.change(description, {target: {value: 'abc'}})
         expect(description).toHaveValue('abc')
-
+        expect(saveButton).not.toHaveAttribute('disabled')
     })
+
+    it('disables the save button if the description is deleted', () => {
+        const saveButton = getByText('Save')
+        const description = getByLabelText('Description:')
+
+        fireEvent.change(description, {target: {value: 'abc'}})
+        expect(description).toHaveValue('abc')
+        expect(saveButton).not.toHaveAttribute('disabled')
+
+        fireEvent.change(description, {target: {value: ''}})
+        expect(description).toHaveValue('')
+        expect(saveButton).toHaveAttribute('disabled')
+    })
+
 })
