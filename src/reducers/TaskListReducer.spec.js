414a9b3a8f777fe70efdb211a700f5e9cd5484ea
Neil Froggatt <neilfr@live.com>
2022-06-10 16:59:49 -0400

    reducer create and delete tests

--- src/reducers/TaskListReducer.spec.js
+++ src/reducers/TaskListReducer.spec.js
@@ -28,4 +28,47 @@ describe('task list reducer tests', () => {
             tasks: [updatedTask]
         })
     })
+
+    it('creates task', () => {
+        const initialState = {
+            nextTaskId:0,
+            tasks:[]
+        }
+        const newTask = {
+            id: null,
+            priority: 1,
+            description: "new task description",
+            complete: false
+        }
+
+        const newState = TaskListReducer(initialState, {type:'create', data:newTask})
+
+        expect(newState).toEqual({
+            nextTaskId: initialState.nextTaskId+1,
+            tasks: [{...newTask, id:0}]
+        })
+    })
+
+    it('deletes task', () => {
+        const originalTaskId = 0
+        const nextTaskId = originalTaskId+1
+        const originalTask = {
+            id: originalTaskId,
+            priority: 1,
+            description: "old description",
+            complete: false
+        }
+        const initialState = {
+            nextTaskId:nextTaskId,
+            tasks:[originalTask]
+        }
+
+        const newState = TaskListReducer(initialState, {type:'delete', data:originalTask})
+
+        expect(newState).toEqual({
+            nextTaskId: nextTaskId,
+            tasks:[]
+        })
+
+    })
 })
