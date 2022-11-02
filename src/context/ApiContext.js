9b8bf99a0927077d69e3e5334e7ac6e3de6bf465
Neil Froggatt <neilfr@live.com>
2022-11-02 17:19:41 -0400

    add client side route guard to client side routes

--- src/context/ApiContext.js
+++ src/context/ApiContext.js
@@ -19,24 +19,27 @@ export const ApiProvider = ({children}) => {
     }
 
     const getStages = async () => {
-        const response = await http.get("http://localhost:8000/api/stages")
+        const response = await http.get("/api/stages")
         return response.data
     }
 
     const updateTask = async (taskId, payload) => {
-        const updatedTask = await http.patch(`http://localhost:8000/api/tasks/${taskId}`,payload)
+        const updatedTask = await http.patch(`/api/tasks/${taskId}`,payload)
         return updatedTask.data.data
     }
+
     const destroyTask = async (taskId) => {
-        const response = await http.delete(`http://localhost:8000/api/tasks/${taskId}`)
+        const response = await http.delete(`/api/tasks/${taskId}`)
         return response.data.data
     }
+
     const createTask = async (payload) => {
-        const createdTask = await http.post(`http://localhost:8000/api/tasks/`, payload)
+        const createdTask = await http.post(`/api/tasks/`, payload)
         return createdTask.data.data
     }
+
     const getTasks = async () => {
-        const response = await http.get("http://localhost:8000/api/tasks")
+        const response = await http.get("/api/tasks")
         return response.data
     }
 
